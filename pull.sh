2b8bc6d0d96a1b925312cacdfc51327d329c934d
Erlend Tobiassen <erlend.tobiassen@gmail.com>
2019-02-09 17:43:57 +0100

    Prefer github link over branch for subtrees

--- pull.sh
+++ pull.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 git pull
-git subtree pull --prefix home/.config config-home master --squash
-git subtree pull --prefix home/.local local-home master --squash
+git subtree pull --prefix home/.config git@github.com:regiontog/config-home.git master --squash
+git subtree pull --prefix home/.local git@github.com:regiontog/local-home.git master --squash
